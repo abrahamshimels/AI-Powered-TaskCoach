58cb5b3f38551f491bda5d784d5c2c25b6985007
abrahamshimels <shimelsabraham123@gmail.com>
2026-04-27 15:52:09 +0300

    deploy: use deployed API

--- frontend/AI-Coach/src/api/axios.js
+++ frontend/AI-Coach/src/api/axios.js
@@ -1,7 +1,7 @@
 import axios from "axios";
 
 const api = axios.create({
-  baseURL: "http://localhost:5000/api", // your backend base URL
+  baseURL: "https://ai-powered-taskcoach.onrender.com/api", // your backend base URL
   withCredentials: true, // optional
 });
 
